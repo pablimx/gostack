7967f398922e1642c34e6dbed340529b4e1a4520
Pablo Resende <pablim@live.co.uk>
2020-09-01 13:52:10 -0300

    applying state and immutability

--- nivel-1/frontend/src/App.js
+++ nivel-1/frontend/src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
+/**
+ * Componente
+ * Propriedade
+ * Estado e Imutabilidade
+ */
+
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  // useState retorna um array com 2 posições
+  //
+  // 1ª com a posição contendo o valor inicial do array
+  // 2ª com uma função para atualizar o valor do array
+
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`);
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage" >
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects" >
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
